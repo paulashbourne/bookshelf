6cfd9f2966f88d4af8f28bfdb7ff5cbecb88dc3d
Paul Ashbourne <paul.ashbourne@gmail.com>
2016-09-23 16:50:17 -0400

    Update production, development DB config

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 export default {
   env: 'development',
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  db: 'mongodb://localhost/express-mongoose-es6-rest-api-development',
+  db: 'mongodb://heroku_xqqjc3j3:1j3seflrrj8e97on9cep03e6d0@ds023435.mlab.com:23435/heroku_xqqjc3j3',
   port: 3000
 };
