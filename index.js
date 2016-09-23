c98262ba25f6af0abf4acf4dde055591d4cdde8f
Paul Ashbourne <paul.ashbourne@gmail.com>
2016-09-23 16:22:14 -0400

    Update README, package.json, index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.db}`);
 });
 
-const debug = require('debug')('express-mongoose-es6-rest-api:index');
+const debug = require('debug')('bookshelf:index');
 
 // listen on port config.port
 app.listen(config.port, () => {
