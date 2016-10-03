161de26820f4266dd0dfb5919a54ef153e3b0945
Paul Ashbourne <paul.ashbourne@gmail.com>
2016-10-02 20:17:33 -0400

    Implemented book search

--- server/routes/search.js
+++ server/routes/search.js
@@ -4,13 +4,13 @@ import GoogleBooks from '../helpers/google_books';
 const router = express.Router(); // eslint-disable-line new-cap
 
 /** GET /api/search - Search books from Google Books */
-router.get('/', (req, res) =>
+router.get('/', (req, res) => {
   let {q, field, offset, limit} = req.query;
   GoogleBooks.search(q, {field, offset, limit}, function(err, results) {
     if (err)
       res.send(err);
     res.json(results);
   });
-);
+});
 
 export default router;
--- server/tests/search.test.js
+++ server/tests/search.test.js
@@ -5,17 +5,15 @@ import app from '../../index';
 
 chai.config.includeStack = true;
 
-describe('## User APIs', () => {
+describe('## Book Search APIs', () => {
 
   describe('# GET /api/search', () => {
     it('should find some books', (done) => {
       request(app)
-        .get(`/api/search?q="cracking%20the%20coding%20interview`)
+        .get(`/api/search?q="cracking%20the%20coding%20interview&limit=5`)
         .expect(httpStatus.OK)
         .then((res) => {
-          debugger;
-          //expect(res.body.username).to.equal(user.username);
-          //expect(res.body.mobileNumber).to.equal(user.mobileNumber);
+          expect(res.body.length).to.equal(5);
           done();
         })
         .catch(done);
